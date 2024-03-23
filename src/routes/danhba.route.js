6401c5ec2659cb6b4e246b510b4f6f9a0f544848
Lynn <lynn@digityze.asia>
2024-03-23 16:05:16 +0700

    tìm kiếm danh bạ theo tên/số điện thoại

--- src/routes/danhba.route.js
+++ src/routes/danhba.route.js
@@ -6,6 +6,7 @@ const danhbaController =   require('../controllers/danhba.controller');
 
 router.post('/addContact', danhbaController.add);
 router.get('/getAllContact/:chudanhba', danhbaController.getAllContact);
+router.get('/findByUsernameOrPhone/:id', danhbaController.findByUsernameOrPhone);
 router.put('/deleteContact/:madanhba', danhbaController.deleteContact);
 
 module.exports = router;
